0c3f923ddba564a355c4487f59f2cc3233f04b17
Apapora <47843562+Apapora@users.noreply.github.com>
2024-03-11 19:03:13 +0800

    Aboutme txt edit

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -8,10 +8,13 @@ function AboutMe() {
   const [message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState(false); // state to track message received
+  const [tooManyRequests, setTooManyRequests] = useState(false);
 
   const handleMessageChange = (e) => {
     setMessage(e.target.value);
     setErrorMessage(""); // Clear any previous error messages when typing
+    setTooManyRequests(false); // Reset tooManyRequests state
+    setMessageReceived(false);
   };
 
   const handleSubmit = async (e) => {
@@ -35,8 +38,8 @@ function AboutMe() {
       // Handle error
       if (error.response && error.response.status === 429) {
         // Handle 429 error (Too Many Requests)
-        // Display custom message to the user
-        setErrorMessage("My answering machine is currently full. Please try again tomorrow");
+        setTooManyRequests(true)
+        //setErrorMessage("My answering machine is currently full. Please try again tomorrow");
       } else {
         // Handle other errors
         setErrorMessage("An error occurred while processing your request.");
@@ -48,8 +51,6 @@ function AboutMe() {
   return (
     <section className="section has-background-info" id="aboutMe">
       <div className="container has-text-centered">
-      
-
         <figure className="image container is-180x180">
           <img
             width="180px"
@@ -63,11 +64,11 @@ function AboutMe() {
         <p className="subtitle is-4 has-text-white has-text-weight-bold">
           {Resume.basics.x_title}
         </p>
-        <div className="subtitle is-5 has-text-white has-text-weight-light summary-text" dangerouslySetInnerHTML={{__html: Resume.basics.summary}}>
+        <div className="subtitle is-5 has-text-white has-text-weight-light" dangerouslySetInnerHTML={{__html: Resume.basics.summary}}>
         </div>
         <div className="subtitle is-5 has-text-white has-text-weight-light summary-text">
           <p>
-            Welcome to my site. You can reach me with 
+            You can reach me via the social links above, or leave me a message on my answering machine below. It'll travel the cloud, convert to audio, and play on my IoT device when I'm home.
           </p>
         </div>
         {messageReceived ? ( // Conditional rendering based on messageReceived state
@@ -75,28 +76,35 @@ function AboutMe() {
             Message received!
           </p>
         ) : (
-          <form onSubmit={handleSubmit}>
-            <div className="field">
-              <label className="label has-text-white">Enter your message:</label>
-              <div className="control">
-                <textarea
-                  className="textarea"
-                  value={message}
-                  onChange={handleMessageChange}
-                  placeholder="Type your message here..."
-                  rows="4"
-                ></textarea>
+          tooManyRequests ? (
+            <p className="subtitle is-4 has-text-white has-text-weight-bold">
+              The answering machine is full. Please try again tomorrow.
+            </p>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <div className="field">
+                <label className="label has-text-white">Enter your message:</label>
+                <div className="control">
+                  <textarea
+                    className="textarea"
+                    value={message}
+                    onChange={handleMessageChange}
+                    placeholder="Type your message here...(between 5-50 characters)"
+                    rows="2"
+                  ></textarea>
+                </div>
+                {errorMessage && <p className="help is-danger">{errorMessage}</p>}
               </div>
-              {errorMessage && <p className="help is-danger">{errorMessage}</p>}
-            </div>
-            <div className="field">
-              <div className="control">
-                <button type="submit" className="button is-info">
-                  Submit
-                </button>
+              <div className="field">
+                <div className="control">
+                  <button type="submit" className="button is-info">
+                    Submit
+                  </button>
+                </div>
+                <br />
               </div>
-            </div>
-          </form>
+            </form>
+          )
         )}
         <div className="container interests">
           <div className="field is-grouped is-grouped-multiline has-text-centered">
